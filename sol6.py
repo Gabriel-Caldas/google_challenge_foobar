f25ce8c878e81c5554c5febc00ff36b13dba672e
Gabriel Caldas <79205962+Gabriel-Caldas@users.noreply.github.com>
2022-10-18 23:01:09 -0300

    update_solution6-complete

--- sol6.py
+++ sol6.py
@@ -1,13 +1,130 @@
-from sys import path
+"""
+Prepare the Bunnies' Escape
+===========================
+
+You're awfully close to destroying the LAMBCHOP doomsday device and freeing Commander Lambda's bunny workers, but once they're free of the work duties the bunnies are going to need to escape Lambda's space station via the escape pods as quickly as possible. Unfortunately, the halls of the space station are a maze of corridors and dead ends that will be a deathtrap for the escaping bunnies. Fortunately, Commander Lambda has put you in charge of a remodeling project that will give you the opportunity to make things a little easier for the bunnies. Unfortunately (again), you can't just remove all obstacles between the bunnies and the escape pods - at most you can remove one wall per escape pod path, both to maintain structural integrity of the station and to avoid arousing Commander Lambda's suspicions. 
+
+You have maps of parts of the space station, each starting at a work area exit and ending at the door to an escape pod. The map is represented as a matrix of 0s and 1s, where 0s are passable space and 1s are impassable walls. The door out of the station is at the top left (0,0) and the door into an escape pod is at the bottom right (w-1,h-1). 
+
+Write a function solution(map) that generates the length of the shortest path from the station door to the escape pod, where you are allowed to remove one wall as part of your remodeling plans. The path length is the total number of nodes you pass through, counting both the entrance and exit nodes. The starting and ending positions are always passable (0). The map will always be solvable, though you may or may not need to remove a wall. The height and width of the map can be from 2 to 20. Moves can only be made in cardinal directions; no diagonal moves are allowed.
+
+
+"""
+import time
+
+from collections import defaultdict
+import itertools
 import numpy as np
 
 
-map =  np.array([[0, 1, 1, 0], [0, 0, 0, 1], [1, 1, 0, 0], [1, 1, 1, 0]])
-map = np.array([[0, 0, 0, 0, 0, 0], [1, 1, 1, 1, 1, 0], [0, 0, 0, 0, 0, 0], [0, 1, 1, 1, 1, 1], [0, 1, 1, 1, 1, 1], [0, 0, 0, 0, 0, 0]])
+def get_edges(map):
+    """Takes map and returns list of links for each origin node (first node = 1)"""
+    map = np.array(map)
+    h = map.shape[0]
+    w = map.shape[1]
+    left_corner = w
+    right_corner =  (h-1)*w+1 
+    edges_list = []
+    # loop of conection conditions for each kind of node 
+    for i in range(1,h*w):
+        if i == left_corner:
+            edge = [[str(i), str(i+w)]]
+        elif i == right_corner:
+            edge = [[str(i), str(i+1)]]
+        elif w > i >1:
+            edge = [[str(i), str(i+1)], [str(i), str(i+w)]]
+        elif i%w == 0 and i != w:
+            edge = [[str(i), str(i-1)], [str(i), str(i+w)]]
+        elif (i-1)%w == 0:
+            edge = [[str(i), str(i+1)], [str(i), str(i+w)]]
+        elif (h*w) > i > ((h-1)*w+1):
+            edge = [[str(i), str(i+1)], [str(i), str(i-w)]]
+        else:
+            edge = [[str(i), str(i-1)], [str(i), str(i+1)], [str(i), str(i-w)], [str(i), str(i+w)] ]
+        edges_list.extend(edge)
+    
+    return edges_list
 
-h = map.shape[0]
-w = map.shape[1]
+def get_weights(map):
+    """Takes map as matrix and returns each node weight"""
+    weights = {}
+    map = np.array(map)
+    maze_T = map.reshape(1, -1)[0]
+    for i, weight in enumerate(maze_T):
+        weights[str(i+1)] = weight
+    
+    return weights
 
-min_legth = (h-1)+(w-1)
+# func to build the graph
+def build_graph(edges_list):
+    """Takes nodes links list and returns the graph table without the weights"""
+    # nodes conections (origin, destiny, weigth)
+    edges = edges_list
 
-directions = 2
+    # graph as a defaultdict object
+    graph = defaultdict(list)
+
+    # create iteractions
+    for edge in edges:
+        # start and final nodes
+        sn, fn = edge
+        graph[sn].append(fn)
+
+    return graph
+
+def solution(map):
+    """Takes map and returns the length of the shortest allowed path from map[0][0] to map[h-1][w-1]"""
+    edges_list = get_edges(map)
+    weights = get_weights(map)
+    graph = build_graph(edges_list)
+    start = str(1) #start point 
+    end = str(len(map) * len(map[0])) #end point (w-1, h-1)
+
+    path_len = 1
+    curr_nodes = [[start, weights[start], path_len]] # node info through path
+    parent_len = 0
+    while True:
+        nxt_nodes = []
+        for curr_node in curr_nodes:
+            parent_node_id = curr_node[0]
+            parent_weight = curr_node[1]
+            parent_len = curr_node[2]
+            parent_len += 1
+            if end in graph[parent_node_id]: # reached the end
+                return parent_len
+            for children_node in graph[parent_node_id]:
+                path_weight = parent_weight+weights[children_node]
+                if path_weight <= 1: # can have one '1' through my way 
+                    nxt_nodes.append([children_node, path_weight, parent_len])
+        nxt_nodes.sort()
+        curr_nodes = list(unique for unique, _ in itertools.groupby(nxt_nodes)) # same point at same weight are the same for the purpose
+
+
+
+
+
+# map = [[0, 1, 1, 0], 
+#         [0, 0, 0, 1], 
+#         [1, 1, 0, 0], 
+#         [1, 1, 1, 0]]
+# map = [[0, 0, 0, 0, 0, 0], 
+#         [1, 1, 1, 1, 1, 0], 
+#         [0, 0, 0, 0, 0, 0], 
+#         [0, 1, 1, 1, 1, 1], 
+#         [0, 1, 1, 1, 1, 1], 
+#         [0, 0, 0, 0, 0, 0]]
+
+# edges_list = get_edges(map)
+# weights = get_weights(map)
+# graph = build_graph(edges_list)
+
+# start = str(1)
+# end = str(len(map) * len(map[0]))
+# # print(graph)
+# print(path_length(graph, weights, start, end))
+
+t0 = time.time()
+map = np.zeros([20, 20])
+
+print(solution(map))
+print(f"Tempo Total: {time.time() - t0}")
--- tests3.py
+++ tests3.py
@@ -1,5 +1,44 @@
 from collections import defaultdict
+import itertools
+import numpy as np
+
+
 import time
+
+def get_edges(maze):
+    maze = np.array(maze)
+    h = maze.shape[0]
+    w = maze.shape[1]
+    left_corner = w
+    right_corner =  (h-1)*w+1 
+    edges_list = []
+    for i in range(1,h*w):
+        if i == left_corner:
+            edge = [[str(i), str(i+w)]]
+        elif i == right_corner:
+            edge = [[str(i), str(i+1)]]
+        elif w > i >1:
+            edge = [[str(i), str(i+1)], [str(i), str(i+w)]]
+        elif i%w == 0 and i != w:
+            edge = [[str(i), str(i-1)], [str(i), str(i+w)]]
+        elif (i-1)%w == 0:
+            edge = [[str(i), str(i+1)], [str(i), str(i+w)]]
+        elif (h*w) > i > ((h-1)*w+1):
+            edge = [[str(i), str(i+1)], [str(i), str(i-w)]]
+        else:
+            edge = [[str(i), str(i-1)], [str(i), str(i+1)], [str(i), str(i-w)], [str(i), str(i+w)] ]
+        edges_list.extend(edge)
+    
+    return edges_list
+
+def get_weights(maze):
+    weights = {}
+    maze = np.array(maze)
+    maze_T = maze.reshape(1, -1)[0]
+    for i, weight in enumerate(maze_T):
+        weights[str(i+1)] = weight
+    
+    return weights
 
 # func to build the graph
 def build_graph(edges_list):
@@ -14,126 +53,65 @@ def build_graph(edges_list):
         # start and final nodes
         sn, fn = edge
 
-        # create connections as undirect graph
         graph[sn].append(fn)
-        # graph[fn].append(sn)
 
     return graph
 
-# test
-# edges_list = [['1', '2'], ['1', '6'], #+1/+5
-#              ['2', '3'], ['2', '7'], #+1/+5
-#              ['6', '7'], ['6', '11'],
-#              ['3', '4'], ['3', '8'],
-#              ['7', '8'], ['7', '12'],
-#              ['11', '12'], ['11', '16'], #+1/+5
-#              ['4', '5'], ['4', '9'],
-#              ['8', '7'], ['8', '9'], ['8', '3'], ['8', '13'],
-#              ['12', '11'], ['12', '13'], ['12', '7'], ['12', '17'],
-#              ['16', '17'], ['16', '21'],
-#              ['5', '10'],
-#              ['9', '8'], ['9', '10'], ['9', '4'], ['9', '14'],
-#              ['13', '12'], ['13', '14'], ['13', '8'], ['13', '18'],
-#              ['17', '16'], ['17', '18'], ['17', '12'], ['17', '22'],
-#              ['21', '22'], 
-#              ['10', '9'], ['10', '15'],
-#              ['14', '13'], ['14', '15'], ['14', '9'], ['14', '19'],
-#              ['18', '17'], ['18', '19'], ['18', '13'],['18', '23'],
-#              ['22', '23'], ['22', '17'], #+1/-5
-#              ['15', '14'], ['15', '20'],
-#              ['19', '18'], ['19', '20'], ['19', '14'], ['19', '24'],
-#              ['23', '24'], ['23', '18'],
-#              ['20', '25'],
-#              ['24', '25']]
-
-# weights = {'1' : 0, '2' : 1, '3' : 1, '4' : 1, '5' : 1 ,
-#            '6' : 1, '7' : 1, '8' : 0, '9' : 0, '10' : 0,
-#            '11' : 0, '12' : 1, '13' : 0, '14' : 1, '15' : 0,
-#            '16' : 0, '17' : 1, '18' : 0, '19' : 1, '20' : 0,
-#            '21' : 0, '22' : 0, '23' : 0, '24' : 1, '25' : 0}
-
-# [[0, 1, 1, 0], [0, 0, 0, 1], [1, 1, 0, 0], [1, 1, 1, 0]]
-# edges_list = [['1', '2'], ['1', '5'], #+1/+h
-#              ['2', '3'], ['2', '6'], #+1/+4
-#              ['5', '6'], ['5', '9'],
-#              ['3', '4'], ['3', '7'],
-#              ['6', '7'], ['6', '10'],
-#              ['9', '10'], ['9', '13'], #+1/+h
-#              ['4', '8'], 
-#              ['7', '6'], ['7', '8'], ['7', '3'], ['7', '11'],
-#              ['10', '9'], ['10', '11'], ['10', '6'], ['10', '14'],
-#              ['13', '14'],
-#              ['8', '7'], ['8', '12'], 
-#              ['11', '12'], ['11', '15'],
-#              ['14', '15'], ['14', '10'],
-#              ['12', '16'],
-#              ['15', '16']]
-
-# weights = {'1' : 0, '2' : 1, '3' : 1, '4' : 0, 
-#            '5' : 0 , '6' : 0, '7' : 0, '8' : 1, 
-#            '9' : 1, '10' : 1, '11' : 0, '12' : 0, 
-#            '13' : 1, '14' : 1, '15' : 1, '16' : 0}
-
-# [[0, 0, 0, 0, 0, 0], 
-#  [1, 1, 1, 1, 1, 0], 
-#  [0, 0, 0, 0, 0, 0], 
-#  [0, 1, 1, 1, 1, 1], 
-#  [0, 1, 1, 1, 1, 1], 
-#  [0, 0, 0, 0, 0, 0]]
-
-weights = {'1' : 0, '2' : 0, '3' : 0, '4' : 0, '5' : 0 , '6' : 0, 
-           '7' : 1, '8' : 1, '9' : 1, '10' : 1, '11' : 1, '12' : 0, 
-           '13' : 0, '14' : 0, '15' : 0, '16' : 0, '17' : 0, '18' : 0, 
-           '19' : 0, '20' : 1, '21' : 1, '22' : 1, '23' : 1, '24' : 1, 
-           '25' : 0, '26' : 1, '27' : 1, '28' : 1, '29' : 1, '30' : 1,
-           '31' : 0, '32': 0, '33' : 0, '34' : 0, '35' : 0, '36' : 0}
 
 
+def solution(maze):
+    edges_list = get_edges(maze)
+    weights = get_weights(maze)
+    graph = build_graph(edges_list)
+    start = str(1)
+    end = str(len(maze) * len(maze[0]))
 
-
-
-
-
-def weighted_nodes(curr_weight, nodes_list):
-    for node in nodes_list:
-        node[1] = node[1] + curr_weight
-    
-    return nodes_list
-
-def path_length(graph, weights,start, end):
+    # graph, weights,start, end):
     path_len = 1
     curr_nodes = [[start, weights[start], path_len]]
     parent_len = 0
-    prev_nodes = [None]
-    count = 0
     while True:
         nxt_nodes = []
         for curr_node in curr_nodes:
-            # if input() == 'q':
-                # return 'end'
-            # print(f"{curr_node}/{curr_nodes}")
-            
             parent_node_id = curr_node[0]
             parent_weight = curr_node[1]
             parent_len = curr_node[2]
             parent_len += 1
+            if end in graph[parent_node_id]:
+                return parent_len
             for children_node in graph[parent_node_id]:
-                # parent_len +=1 
-                if end in children_node:
-
-                    print('CHEGOU!')
-                    # print(parent_len)
-                    # print(graph[parent_node_id])
-                    return parent_len 
                 path_weight = parent_weight+weights[children_node]
                 if path_weight <= 1:
                     nxt_nodes.append([children_node, path_weight, parent_len])
-        curr_nodes = nxt_nodes
+        nxt_nodes.sort()
+        curr_nodes = list(unique for unique, _ in itertools.groupby(nxt_nodes))
 
 
-graph = build_graph(edges_list)
-# print(graph)
+
+
+
+# maze = [[0, 1, 1, 0], 
+#         [0, 0, 0, 1], 
+#         [1, 1, 0, 0], 
+#         [1, 1, 1, 0]]
+# maze = [[0, 0, 0, 0, 0, 0], 
+#         [1, 1, 1, 1, 1, 0], 
+#         [0, 0, 0, 0, 0, 0], 
+#         [0, 1, 1, 1, 1, 1], 
+#         [0, 1, 1, 1, 1, 1], 
+#         [0, 0, 0, 0, 0, 0]]
+
+# edges_list = get_edges(maze)
+# weights = get_weights(maze)
+# graph = build_graph(edges_list)
+
+# start = str(1)
+# end = str(len(maze) * len(maze[0]))
+# # print(graph)
+# print(path_length(graph, weights, start, end))
+
 t0 = time.time()
-print(path_length(graph, weights, '1', '16'))
-print(f"Tempo: {time.time() - t0}")
-# BFS_SP(graph, 'A', 'D')
+maze = np.zeros([40, 40])
+
+print(solution(maze))
+print(f"Tempo Total: {time.time() - t0}")
